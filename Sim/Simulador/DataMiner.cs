a468a22247a66c9823e3f7c4011cb49f7ca10616
Alexis Quintero <alexisquintero@hotmail.com.ar>
2018-08-22 22:05:51 -0300

    Start simulator

---
+++ Sim/Simulador/DataMiner.cs
@@ -0,0 +1,23 @@
+﻿namespace Simulador
+{
+  /* Helper class used to determine needed values for the simulation
+   */
+  class DataMiner //TODO: Might be a good idea to parallelize data mining
+  {
+    /* Determines the number of clients in a certain period (a day, week, month
+     * , year, etc.)
+     */
+    private int numberClientsPeriod()
+    {
+      return 0; //TODO: Implement
+    }
+
+    /* Determines the amount of a given product sold on a certain period (a day
+     * , week, month, year, etc.)
+     */
+    private decimal amountSalesProductPeriod()
+    {
+      return 0; //TODO: Implement
+    }
+  }
+}
---
+++ Sim/Simulador/Generators/NormalDIstribution.cs
@@ -0,0 +1,19 @@
+﻿using MathNet.Numerics.Distributions;
+using MathNet.Numerics.Random;
+
+namespace Simulador.Generators
+{
+  class NormalDIstribution
+  {
+    double orderSize(double mean, double stddev)
+    {
+      Normal normal = new Normal(mean, stddev);
+      return normal.Sample();
+    }
+    double timeBetweenOrder(double mean, double stddev)
+    {
+      Normal normal = new Normal(mean, stddev);
+      return normal.Sample();
+    }
+  }
+}
