cdd70ea241d2e9b54a9aa70219700e67c4e0da23
Alexis Quintero <alexisquintero@hotmail.com.ar>
2018-08-26 15:33:09 -0300

    Add some simulator functions

--- Sim/Simulador/Inventory.cs
+++ Sim/Simulador/Inventory.cs
@@ -1,5 +1,5 @@
 ﻿using Simulador.Events;
-using System.Collections.Generic;
+using Simulador.Utils;
 
 namespace Simulador
 {
@@ -9,12 +9,21 @@ namespace Simulador
     //Determine order frequency probability distribution
     //Determine order size probability distribution
 
-    private static double inventory;            //Actual inventory value
-    private static double demand = 0;           //Satisfied demand
+    private static double inventory;  //Inventory value at start of simulation
+    //TODO: Manage inventory refill, different modes, etc.
+    private static double totalDemand = 0;      //Total demanded ammount
+    private static double satisfiedDemand = 0;  //Satisfied demand
     private static double missedDemand = 0;     //Not satisfied demand
     private static double clock = 0;            //Simulation time
     private static double endTime;              //Simulation end time  
-    private static List<Order> events = new List<Order>(); //Event list
+    private static LinkedList<Order> eventList = 
+      new LinkedList<Order>();                  //Event list
+    private static LinkedList<Order> events = 
+      new LinkedList<Order>();                  //Every event list
+
+    //Statistics
+    private static int totalNumberOfOrders = 0;
+    private static int numberOfOrdersNotEnoughStock = 0;
     private void Simulation(
       double orderSizeMean,
       double orderSizeStdDev,
@@ -23,14 +32,13 @@ namespace Simulador
     {
       Initialization();
       while (clock < endTime)
-      { 
-        //Generate new event
-        Order order = new Order(
+      {
+        Statistics();
+        GenerateEvent(
           orderSizeMean, 
           orderSizeStdDev, 
           timeBetweenOrdersMean, 
           timeBetweenOrderStdDev);
-        //Advance clock to next event
       }
     }
 
@@ -47,9 +55,60 @@ namespace Simulador
       endTime = pEndTime;
       
     }
-    private static void Initialization()
+    private static void Initialization(
+      double endOfSimulationTime,
+      double startOfSimulationTime,
+      double initialInventory,
+      double osMean, 
+      double osStdDev, 
+      double tboMean, 
+      double tboStdDev)
     {
+      //Set the end of simulation time
+      endTime = endOfSimulationTime;
+      //Set the start of simultion time
+      clock = startOfSimulationTime;
       //Generate first event
+      eventList.Add(new Order(osMean, osStdDev, tboMean, tboStdDev));
+      //Advance clock of simulation to first event
+      clock += eventList.Head().Time;
+    }
+    private static void Statistics()
+    {
+      totalNumberOfOrders += 1;
+      if (inventory < eventList.Head().Ammount)
+      {
+        inventory = 0;
+        numberOfOrdersNotEnoughStock += 1;
+        missedDemand += inventory == 0 ? 
+          eventList.Head().Ammount : inventory - eventList.Head().Ammount;
+      }
+      else
+      {
+        satisfiedDemand += eventList.Head().Ammount;
+      }
+      totalDemand += eventList.Head().Ammount;
+    }
+    private static void GenerateEvent(
+      double orderSizeMean, 
+      double orderSizeStdDev, 
+      double timeBetweenOrdersMean, 
+      double timeBetweenOrderStdDev)
+    {
+      //Add event to every event list
+      events.Add(eventList.Head());
+      //Remove event from event list
+      eventList = eventList.Tail();
+      //Generate new event
+      Order order = new Order(
+        orderSizeMean,
+        orderSizeStdDev,
+        timeBetweenOrdersMean,
+        timeBetweenOrderStdDev);
+      //Add event to event list
+      eventList.Add(order);
+      //Advance clock to next event
+      clock += eventList.Head().Time;
     }
   }
 }
