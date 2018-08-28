1800fb72d3fd10147513ab66f527ec934192110d
Alexis Quintero <alexisquintero@hotmail.com.ar>
2018-08-28 20:04:23 -0300

    Add some Queue functions

--- Sim/Simulador/Queue.cs
+++ Sim/Simulador/Queue.cs
@@ -1,21 +1,39 @@
 ﻿using Simulador.Utils;
 using Simulador.Events;
+using static Simulador.Utils.Enumerators;
 
 namespace Simulador
 {
   class Queue : SimBase
   {
+    private Server[] servers;
     private void Simulation(
+      double timeStartSimulation, 
+      double timeEndSimulation, 
+      double numberOfServers,
       double arrivalMean, 
       double arrivalStdDev, 
       double departureMean, 
       double departureStdDev)
     {
-      Initialization();
+      Initialization(
+        timeStartSimulation, 
+        timeEndSimulation, 
+        numberOfServers, 
+        arrivalMean, 
+        arrivalStdDev, 
+        departureMean, 
+        departureStdDev);
+      while (clock < endTime)
+      {
+        Statistics();
+        GenerateEvent();
+      }
     }
     private void Initialization(
       double startOfSimulationTime, 
       double endOfSimulationTime,
+      double numberOfServers,
       double arrivalTimeMean, 
       double arrivalTimeStdDev, 
       double departureTimeMean, 
@@ -26,9 +44,22 @@ namespace Simulador
       //Set the end of simulation time
       endTime = endOfSimulationTime;
       //Set next arrival as next event
-      nextEvent = Enumerators.EventEnum.Arrival;
-      //Generate first event
-      eventList[(int)nextEvent] = (new Arrival(arrivalTimeMean, arrivalTimeStdDev));
+      nextEvent = EventEnum.Arrival;
+      //Generate first events
+      eventList[(int)nextEvent] = 
+        new Arrival(arrivalTimeMean, arrivalTimeStdDev);
+      eventList[(int)EventEnum.Departure] =
+        new Departure(departureTimeMean, departureTimeStdDev);
+      //Advance clock of simulation to first event
+      if (eventList[(int)nextEvent] is Arrival a)
+      {
+        clock += a.Time;
+      }
+      //Create servers
+      for (int i = 0; i < numberOfServers; i++)
+      {
+        servers[i] = new Server();
+      }
     }
   }
 }
---
+++ Sim/Simulador/Server.cs
@@ -0,0 +1,16 @@
+﻿namespace Simulador
+{
+  class Server
+  {
+    public Server()
+    {
+      free = true;
+      numberOfCustomersServed = 0;
+    }
+    public bool free { get; set; }
+    public int numberOfCustomersServed { get; set; }
+    public double activeTime { get; set; }
+    public void AddCustomerServed() => numberOfCustomersServed += 1;
+    public void AddActiveTime(double time) => activeTime += time;
+  }
+}
