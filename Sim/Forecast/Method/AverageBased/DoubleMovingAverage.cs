15fab4c173efb53b3cddaec427a973cba9891c9b
Alexis Quintero <alexisquintero@hotmail.com.ar>
2018-09-05 20:00:41 -0300

    Add forecast calculation methods

---
+++ Sim/Forecast/Method/AverageBased/DoubleMovingAverage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Forecast.Method.AverageBased
+{
+  class DoubleMovingAverage
+  {
+    //116 
+    public static double[] Calculate(double[] inputValue, int amountOfPeriodToCalculate, int movingAverageTerms)
+    {
+      //Calculate first moving average
+      double[] firstAverage = MovingAverage.Calculate(inputValue, amountOfPeriodToCalculate, movingAverageTerms);
+      //Calculate second moving average
+      double[] secondAverage = MovingAverage.Calculate(firstAverage, amountOfPeriodToCalculate, movingAverageTerms);
+      //Remove first movingAverageTerms - 1 values since they are not use
+      List<double> fixFirstAverage = firstAverage.ToList().Skip(movingAverageTerms - 1).ToList();
+      //Call function to calculate the 'n' next periods
+      return Calculate(fixFirstAverage, secondAverage.ToList(), new List<double>(), movingAverageTerms, 1, amountOfPeriodToCalculate).ToArray<double>();
+    } 
+    private static List<double> Calculate(List<double> firstAverage, List<double> secondAverage, List<double> outputValue, int movingAverageTerms, int index, int amountOfPeriodToCalculate)
+    {
+      if (index > amountOfPeriodToCalculate) return outputValue;
+      double at = 2 * firstAverage.First() - secondAverage.First();
+      double bt = 2 / (movingAverageTerms - 1) * (firstAverage.First() - secondAverage.First());
+      double forecastValue = at + bt * index;
+      outputValue.Add(forecastValue);
+      if (1 == firstAverage.Count)
+      {
+        return Calculate(firstAverage, secondAverage, outputValue, movingAverageTerms, index++, amountOfPeriodToCalculate);
+      }
+      else
+      {
+        firstAverage.RemoveAt(0);
+        secondAverage.RemoveAt(0);
+        return Calculate(firstAverage, secondAverage, outputValue, movingAverageTerms, index, amountOfPeriodToCalculate);
+      }
+    }
+  }
+}
---
+++ Sim/Forecast/Method/AverageBased/SimpleExponentialSmoothing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Forecast.Method.AverageBased
+{
+  class SimpleExponentialSmoothing
+  {
+    public static double[] Calculate(double[] inputValue, double smoothingConstant)
+    {
+      return Calculate(inputValue.ToList(), new List<double>(), smoothingConstant).ToArray();
+    }
+    private static List<double> Calculate(List<double> realValues, List<double> output, double smoothingConstant)
+    {
+      if (0 == realValues.Count) return output;
+      else if (0 == output.Count)
+      {
+        output.Add(realValues.First());
+      }
+      else
+      {
+        double forecastValue = output.Last() + smoothingConstant * (realValues.First() - output.Last());
+        output.Add(forecastValue);
+      }
+      realValues.RemoveAt(0);
+      return Calculate(realValues, output, smoothingConstant);
+    }
+  }
+}
