391cc4209249e5a2d4aec73b06c0bb9c3716ad2b
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2018-09-15 19:06:13 -0300

    Refactor private methods from the forecast project

---
+++ Sim/Forecast/Method/AverageBased/SimpleAverageHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Forecast.Method.AverageBased
+{
+  class SimpleAverageHelper
+  {
+    public static List<double> Calculate(List<double> inputValue, int amountOfPeriodToCalculate)
+    {
+      //Return value if no need to calculate next periods
+      if (0 == amountOfPeriodToCalculate) return inputValue;
+      else
+      {
+        //Calculate next period
+        double nextPeriod = inputValue.Sum() / inputValue.Count;
+        //Add next period to values
+        inputValue.Add(nextPeriod);
+        //Return new values
+        return Calculate(inputValue, amountOfPeriodToCalculate - 1);
+      }
+    }
+  }
+}
--- Sim/Forecast/Method/AverageBased/SimpleExponentialSmoothing.cs
+++ Sim/Forecast/Method/AverageBased/SimpleExponentialSmoothing.cs
@@ -7,22 +7,8 @@ namespace Forecast.Method.AverageBased
   {
     public static double[] Calculate(double[] inputValue, double smoothingConstant)
     {
-      return Calculate(inputValue.ToList(), new List<double>(), smoothingConstant).ToArray();
-    }
-    private static List<double> Calculate(List<double> realValues, List<double> output, double smoothingConstant)
-    {
-      if (0 == realValues.Count) return output;
-      else if (0 == output.Count)
-      {
-        output.Add(realValues.First());
-      }
-      else
-      {
-        double forecastValue = output.Last() + smoothingConstant * (realValues.First() - output.Last());
-        output.Add(forecastValue);
-      }
-      realValues.RemoveAt(0);
-      return Calculate(realValues, output, smoothingConstant);
+      return SimpleExponentialSmoothingHelper.Calculate(
+        inputValue.ToList(), new List<double>(), smoothingConstant).ToArray();
     }
   }
 }
